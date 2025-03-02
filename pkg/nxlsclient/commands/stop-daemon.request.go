67a0a2009f89c3887680a8fad775f29d3c6a2799
Nacho Vazquez <nacho@naxo.dev>
2025-03-02 19:10:19 -0300

    fix: add missing param to stop daemon

--- pkg/nxlsclient/commands/stop-daemon.request.go
+++ pkg/nxlsclient/commands/stop-daemon.request.go
@@ -8,12 +8,22 @@ const (
 	StopNxDaemonRequestMethod = "nx/stopDaemon"
 )
 
+// StopDaemonParams is the parameter structure for stop daemon request
+type StopDaemonParams struct {
+	Force bool `json:"force"` // Option to force stop the daemon
+}
+
 // SendStopNxDaemonRequest sends a request to stop the NX daemon.
 func (c *Commander) SendStopNxDaemonRequest(ctx context.Context) error {
 	var result any
 
-	c.Logger.Debug("Sending stop daemon request")
-	err := c.sendRequest(ctx, StopNxDaemonRequestMethod, nil, &result)
+	// Create parameters with force option
+	params := StopDaemonParams{
+		Force: true, // Force stop to ensure daemon is killed
+	}
+
+	c.Logger.Debugw("Sending stop daemon request with parameters", "params", params)
+	err := c.sendRequest(ctx, StopNxDaemonRequestMethod, params, &result)
 	if err != nil {
 		return err
 	}
