653656e87d4aa2e07d591ed81aa0ad74bcdae97d
Nacho Vazquez <nacho@naxo.dev>
2025-03-23 23:07:59 -0300

    feat: improve the graph recognition

---
+++ tools/gonx/src/graph/types/go-plugin-options.ts
@@ -0,0 +1,9 @@
+// Expected format of the plugin options defined in nx.json
+export interface GoPluginOptions {
+  buildTargetName?: string;
+  testTargetName?: string;
+  runTargetName?: string;
+  tidyTargetName?: string;
+  lintTargetName?: string;
+  tagName?: string;
+}
---
+++ tools/gonx/src/graph/utils/targets/test.ts
@@ -0,0 +1,19 @@
+import { joinPathFragments } from '@nx/devkit';
+
+export const test = (projectRoot: string) => ({
+  executor: 'nx:run-commands',
+  options: {
+    command: 'go test ./...',
+    cwd: projectRoot,
+  },
+  cache: true,
+  inputs: [
+    '{projectRoot}/go.mod',
+    '{projectRoot}/go.sum',
+    joinPathFragments('{projectRoot}', '**', '*.go'),
+    {
+      externalDependencies: ['go'],
+    },
+  ],
+  outputs: [],
+});
